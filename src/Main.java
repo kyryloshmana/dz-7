1c378eaf25ef31b13116c9f8816dfd9b9aec76c1
Шмана Кирило Сергійович <k.shmana@temabit.com>
2023-04-29 18:23:06 +0300

    Homework

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+    Planets planetSun = Planets.Sun;
+    Planets planetMercury = Planets.Mercury;
+    Planets planetVenus = Planets.Venus;
+    Planets planetEarth = Planets.Earth;
+    Planets planetMars = Planets.Mars;
+    Planets planetJupiter = Planets.Jupiter;
+    Planets planetSaturn = Planets.Saturn;
+    Planets planetUranus = Planets.Uranus;
+    Planets planetNeptune = Planets.Neptune;
+
+        System.out.println("The planet " + planetSun + " has a distance from sun is " + planetSun.getDistanceFromSun());
+        System.out.println("The planet " + planetMercury + " has a distance from sun is " + planetMercury.getDistanceFromSun());
+        System.out.println("The planet " + planetVenus + " has a distance from sun is " + planetVenus.getDistanceFromSun());
+        System.out.println("The planet " + planetEarth + " has a distance from sun is " + planetEarth.getDistanceFromSun());
+        System.out.println("The planet " + planetMars + " has a distance from sun is " + planetMars.getDistanceFromSun());
+        System.out.println("The planet " + planetJupiter + " has a distance from sun is " + planetJupiter.getDistanceFromSun());
+        System.out.println("The planet " + planetSaturn + " has a distance from sun is " + planetSaturn.getDistanceFromSun());
+        System.out.println("The planet " + planetUranus + " has a distance from sun is " + planetUranus.getDistanceFromSun());
+        System.out.println("The planet " + planetNeptune + " has a distance from sun is " + planetNeptune.getDistanceFromSun());
+
+
+    }
+}
